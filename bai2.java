1badb19f1862850bcf3afff18ed3dbe0f701892b
HwangLongg <longlongokok2@gmail.com>
2026-01-26 19:18:55 +0700

    Refactor session01_bai2 to Bai2 with category input

--- bai2.java
+++ bai2.java
@@ -1,27 +1,27 @@
-package PTIT_CNTT1_IT203A_Session01;
-
 import java.util.Scanner;
 
-public class session01_bai2 {
+public class Bai2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Nhập số ngày chậm trễ: ");
-        int n = sc.nextInt();
-        System.out.print("Nhập số lượng sách mượn: ");
-        int m = sc.nextInt();
+        System.out.print("Nhập mã thể loại (A, B, C, D): ");
+        char code = sc.next().toUpperCase().charAt(0);
 
-        double firstFine = n * m * 5000;
-        double finalFine = firstFine;
-
-        if (n>7 && m>=3){
-            finalFine = firstFine * 1.2;
+        switch (code) {
+            case 'A':
+                System.out.println("Tầng 1: Sách Văn học");
+                break;
+            case 'B':
+                System.out.println("Tầng 2: Sách Khoa học");
+                break;
+            case 'C':
+                System.out.println("Tầng 3: Sách Ngoại ngữ");
+                break;
+            case 'D':
+                System.out.println("Tầng 4: Sách Tin học");
+                break;
+            default:
+                System.out.println("Mã không hợp lệ!");
         }
-
-        boolean blockCard = finalFine > 50000;
-
-        System.out.println("\nTiền phạt gốc: " + firstFine + " VNĐ");
-        System.out.println("Tiền phạt sau điều chỉnh: " + finalFine + " VNĐ");
-        System.out.println("Yêu cầu khóa thẻ: " + blockCard);
     }
 }
