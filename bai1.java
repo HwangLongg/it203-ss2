c54730ca9ea1c5890dd1b754ca600ff553fb510e
HwangLongg <longlongokok2@gmail.com>
2026-01-26 19:18:03 +0700

    Update bai1.java

--- bai1.java
+++ bai1.java
@@ -1,32 +1,23 @@
-package PTIT_CNTT1_IT203A_Session01;
-
 import java.util.Scanner;
 
-public class session01_bai1 {
+public class Bai1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Nhập mã sách: ");
-        String bookID = sc.nextLine();
-        System.out.print("Nhập tên sách: ");
-        String bookName = sc.nextLine();
-        System.out.print("Nhập năm xuất bản: ");
-        int publishYear = sc.nextInt();
-        System.out.print("Nhập giá sách: ");
-        double price = sc.nextDouble();
-        System.out.print("Sách còn trong kho (true/false): ");
-        boolean isAvailable = sc.nextBoolean();
+        System.out.print("Nhập tuổi của bạn: ");
+        int tuoi = sc.nextInt();
+        System.out.print("Nhập số sách đang giữ: ");
+        int soSach = sc.nextInt();
 
-        int bookAge = 2026 - publishYear;
-        String status = isAvailable ? "Còn sách" : "Đã mượn";
-
-        System.out.println("\n===== PHIẾU THÔNG TIN SÁCH =====");
-        System.out.println("Mã sách      : " + bookID);
-        System.out.println("Tên sách     : " + bookName);
-        System.out.println("Năm xuất bản   : " + publishYear);
-        System.out.println("Tuổi thọ: " + bookAge + " năm");
-        System.out.printf("Giá bán    : %.2f\n", price);
-        System.out.println("Tình trạng     : " + status);
-        ;
+        if (tuoi >= 18 && soSach < 3) {
+            System.out.println("Cho phép mượn sách");
+        } else {
+            if (tuoi < 18) {
+                System.out.println("Lý do: Bạn chưa đủ tuổi (phải từ 18 tuổi)");
+            }
+            if (soSach >= 3) {
+                System.out.println("Lý do: Bạn đã mượn quá số lượng cho phép (tối đa 2 cuốn)");
+            }
+        }
     }
 }
