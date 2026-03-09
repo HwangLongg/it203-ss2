57b76cb34ca6e35d00b834ded857aa5e340c87fb
HwangLongg <longlongokok2@gmail.com>
2026-03-10 02:23:36 +0700

    Update bai5.java

--- bai5.java
+++ bai5.java
@@ -1,35 +1,24 @@
-import java.util.Scanner;
+interface UserActions {
+    default void logActivity(String activity) {
+        System.out.println("User log: " + activity);
+    }
+}
 
-public class Bai5 {
+interface AdminActions {
+    default void logActivity(String activity) {
+        System.out.println("Admin log: " + activity);
+    }
+}
+
+class SuperAdmin implements UserActions, AdminActions {
+    @Override
+    public void logActivity(String activity) {
+        UserActions.super.logActivity(activity);
+        AdminActions.super.logActivity(activity);
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int diemUyTin = 100;
-        int ngayTre;
-
-        System.out.println("--- Nhập lịch sử trả sách (Nhập 999 để kết thúc) ---");
-        while (true) {
-            System.out.print("Nhập số ngày trễ: ");
-            ngayTre = sc.nextInt();
-
-            if (ngayTre == 999) break;
-
-            if (ngayTre <= 0) {
-                diemUyTin += 5;
-                System.out.println("Trả đúng hạn: +5 điểm");
-            } else {
-                diemUyTin -= (ngayTre * 2);
-                System.out.println("Trả muộn " + ngayTre + " ngày: -" + (ngayTre * 2) + " điểm");
-            }
-        }
-
-        System.out.println("\nTổng điểm uy tín cuối cùng: " + diemUyTin);
-        System.out.print("Xếp loại: ");
-        if (diemUyTin > 120) {
-            System.out.println("Độc giả Thân thiết");
-        } else if (diemUyTin >= 80) {
-            System.out.println("Độc giả Tiêu chuẩn");
-        } else {
-            System.out.println("Độc giả cần lưu ý");
-        }
+        SuperAdmin superadmin = new SuperAdmin();
+        superadmin.logActivity("Xóa người dùng");
     }
 }
