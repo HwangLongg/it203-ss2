5b6c37f1ee253e0131f105f1519b582757095061
HwangLongg <longlongokok2@gmail.com>
2026-03-10 02:23:11 +0700

    Update bai4.java
    Refactored code to use streams for user handling and removed ID input logic.

--- bai4.java
+++ bai4.java
@@ -1,18 +1,47 @@
-import java.util.Scanner;
- 
+import java.util.Arrays;
+import java.util.List;
+
+class User {
+    private String username;
+    public User() {
+        this.username = "default";
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
+
 public class Bai4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int id;
+        List<User> users = Arrays.asList(
+                new User("Tiến"),
+                new User("Việt"),
+                new User("Thành")
+        );
 
-        do {
-            System.out.print("Nhập mã ID sách mới (số dương): ");
-            id = sc.nextInt();
-            if (id <= 0) {
-                System.out.println("Mã không hợp lệ, hãy nhập lại!");
-            }
-        } while (id <= 0);
+        // Tham chiếu instance method của đối tượng bất kỳ thuộc kiểu cụ thể
+        users.stream()
+                .map(User::getUsername)
+                .forEach(System.out::println);
 
-        System.out.println("Lưu mã sách thành công: " + id);
+        // Tham chiếu instance method của một đối tượng cụ thể
+        users.stream()
+                .map(User::getUsername)
+                .forEach(System.out::println);
+
+        // Tham chiếu Constructor
+        UserFactory factory = User::new;
+        User newUser = factory.create();
+        System.out.println("New user: " + newUser.getUsername());
     }
+}
+
+@FunctionalInterface
+interface UserFactory {
+    User create();
 }
